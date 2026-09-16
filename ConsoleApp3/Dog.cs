efec51bedda6b26411c71d1463f61404377a96b4
Bereg.Levente <Bereg.Levente@C214-04>
2026-09-16 10:59:33 +0200

    Add project files.

---
+++ ConsoleApp3/Dog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Dog
+    {
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public int Age { get; set; }
+        public Dog(string name, string breed, int age)
+        {
+            Name = name;
+            Breed = breed;
+            Age = age;
+        }
+    }
+}
